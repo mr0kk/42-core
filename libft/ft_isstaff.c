315f6760f67e9f7fdb0378e1a722280b423e7165
mr0kk <radoslawmrok@gmail.com>
2024-12-18 16:43:39 +0100

    ft_isstaff update

--- libft/ft_isstaff.c
+++ libft/ft_isstaff.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.c                                            :+:      :+:    :+:   */
+/*   ft_isstaff.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rmrok <rmrok@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 23:49:24 by rmrok             #+#    #+#             */
-/*   Updated: 2024/12/11 23:49:26 by rmrok            ###   ########.fr       */
+/*   Updated: 2024/12/18 16:41:42 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,38 @@
 
 int	ft_isalpha(char c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-		return (1024);
-	return (0);
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
 
 int	ft_isdigit(char c)
 {
-	if (c >= '0' && c <= '9')
-		return (1024);
-	return (0);
+	return (c >= '0' && c <= '9');
 }
 
 int	ft_isalnum(char c)
 {
-	if (ft_isdigit(c) || ft_isalpha(c))
-		return (1024);
-	return (0);
+	return (ft_isdigit(c) || ft_isalpha(c));
 }
 
 int	ft_isprint(char c)
 {
-	if (c > '~' || c < ' ')
-		return (0);
-	return (1024);
+	return (c >= '!' && c <= '~');
 }
 
-size_t	ft_strlen(char *str)
+int main()
 {
-	size_t len;
+	char c;
 
-	len = 0;
-	while (*str++)
-		len++;
-	return (len);
+	c = '1';
+	printf("char\tfunc()\tft_funct()\n");
+	printf("%c\t%d\t%d\n", c, isalpha(c), ft_isalpha(c));
+	c = 'a';
+	printf("%c\t%d\t%d\n", c, isalpha(c), ft_isalpha(c));
+	c = '\t';
+	printf("%c\t%d\t%d\n", c, isprint(c), ft_isprint(c));
+	c = ' ';
+	printf("%c\t%d\t%d\n", c, isprint(c), ft_isprint(c));
+	c = 'd';
+	printf("%c\t%d\t%d\n", c, isprint(c), ft_isprint(c));	
+	return (0);
 }
