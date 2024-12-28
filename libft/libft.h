112aa19fa7bcc16de332ad1dab5968e85705ccb7
mr0kk <radoslawmrok@gmail.com>
2024-12-28 14:31:38 +0100

    ft_memchr done

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 23:49:12 by rmrok             #+#    #+#             */
-/*   Updated: 2024/12/28 12:59:35 by marvin           ###   ########.fr       */
+/*   Updated: 2024/12/28 14:31:15 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ void *ft_memmove(void *dest, const void *src, size_t n);
 size_t ft_strlcpy(char *dest, const char *src, size_t n);
 size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
 int ft_strncmp(const char *s1, const char*s2, size_t n);
+void *ft_memchr(const void *s, int c, size_t n);
 
 
 int ft_tolower(int c);
