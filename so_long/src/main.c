d8153681cd32f3aff0be5ce595f9b41f29146766
Radoslaw Mrok <rmrok@c3r16s3.42warsaw.pl>
2025-04-10 15:13:16 +0200

    learning ow mlx works

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -3,28 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rmrok <rmrok@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 13:02:52 by rmrok             #+#    #+#             */
-/*   Updated: 2025/03/20 00:08:47 by marvin           ###   ########.fr       */
+/*   Updated: 2025/04/10 15:02:49 by rmrok            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+typedef struct s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
 int main(void)
 {
-	void *mlx;
-	void *img;
+	void	*mlx;
+	t_data	img;
+	// void	*mlx_win;
 	
 	mlx = mlx_init();
-	img = mlx_new_image(mlx, 1920, 1080);
+	img.img = mlx_new_image(mlx, 500, 500);
+	// mlx_win = mlx_new_window(mlx, 500, 500, "Hello World!");
+	// mlx_loop(mlx);
 
-	if (!img)
-    {
-        ft_printf("dupa\n");
-        return (1);
-    }
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	
-	ft_printf("hello\n");
 }
