59ae8bc897605d41d11f76c184c4a16fa929ef8f
radek <radoslawmrok@gmail.com>
2026-02-26 15:55:34 +0100

    current version

--- philosophers/philo/include/philo.h
+++ philosophers/philo/include/philo.h
@@ -18,13 +18,5 @@
 #include <pthread.h>
 #include <time.h>
 
-typedef struct s_philo_data
-{
-	int	number_of_philosophers;
-	time_t	time_to_die;
-	time_t	time_to_eat;
-	time_t	time_to_sleep;
-	time_t	number_of_time_each_philo_eat;
-}		t_philo_data;
 
 #endif 
--- philosophers/philo/src/main.c
+++ philosophers/philo/src/main.c
@@ -4,7 +4,7 @@
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmrok <rmrok@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                +#+#su r+#+#+#+   +#+           */
 /*   Created: 2025/10/06 21:20:38 by rmrok             #+#    #+#             */
 /*   Updated: 2025/10/08 20:51:01 by rmrok            ###   ########.fr       */
 /*                                                                            */
@@ -12,41 +12,18 @@
 
 #include "../include/philo.h"
 
-
-void	*test_func(void *arg)
+void *test_func(void *arg)
 {
-	pthread_t this_thread;
-
-	this_thread = pthread_self();
-	printf("Current thread ID: %lu\n",
-		   (unsigned long)this_thread);
-	while (1)
-	{
-		printf("thred is running\n");
-		sleep(1);
-	}	
+	printf("in thread\n");
 	return (NULL);
 }
 
 int main(int argc, char *argv[])
 {
-	t_philo_data data;
-	pthread_t	thred_ptr;
-	// if (!(argc == 5 || argc == 6))
-	// if (!is_input_valid(argc, argv))
-	// 	exit_with_error("Invalid input\n");
-	// check params amount 
-	// check if params are valid
-	// start simulation
-	// close simualtion -> exit	
+	pthread_t thread1;
 
-	pthread_create(&thred_ptr, NULL, test_func, NULL);
-	sleep(5);
-	printf("in main\n");
-	sleep(4);
-	pthread_cancel(thred_ptr);
+	pthread_create(&thread1, NULL, test_func, NULL);
 	
-	pthread_join(thred_ptr, NULL);
-	printf("Main thread finished.\n");
+	pthread_join(thread1, NULL);
 	return (0);
 }
